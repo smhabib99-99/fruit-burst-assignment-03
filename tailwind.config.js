d345356a285c8cda43e8ed6a3ab4e9abfff662e3
smhabib99-99 <habibaust34@gmail.com>
2023-07-26 22:49:35 +0600

    first slider done

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,3 +6,16 @@ module.exports = {
     },
     plugins: [],
   }
+
+  module.exports = {
+    theme: {
+      extend: {
+        backgroundImage: {
+          'hero-pattern': "url('/images/banner1.png')",
+          'hero-pattern': "url('/images/banner2.png')",
+          'hero-pattern': "url('/images/banner3.png')",
+          'hero-pattern': "url('/images/banner4.png')",
+        }
+      }
+    }
+  }
